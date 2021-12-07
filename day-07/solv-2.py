0dff8840a1bdf906f0ca163a291c5f245738ca21
Endre Fejes <fejese@fejese.com>
2021-12-07 11:36:45 +0100

    blacken

--- day-07/solv-2.py
+++ day-07/solv-2.py
@@ -8,12 +8,14 @@ INPUT_FILE_NAME: str = "input"
 with open(INPUT_FILE_NAME, "r") as input_file:
     positions = [int(p) for p in input_file.read().strip().split(",")]
 
+
 def sum1n(n: int) -> int:
     return int((n * (n + 1)) / 2)
 
+
 min_cost: Optional[int] = None
 for aligned_pos in range(min(positions), max(positions)):
-    cost = sum(sum1n(abs(p-aligned_pos)) for p in positions)
+    cost = sum(sum1n(abs(p - aligned_pos)) for p in positions)
     if min_cost is None or cost < min_cost:
         min_cost = cost
 
