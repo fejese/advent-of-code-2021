7e01bdd4546fba8a9ede623e3d4e00c76025dcc1
Endre Fejes <fejese@fejese.com>
2021-12-13 10:17:24 +0000

    day 13

---
+++ day-13/solv-2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import List, Set, Tuple
+
+# INPUT_FILE_NAME: str = "test-input"
+INPUT_FILE_NAME: str = "input"
+
+
+def dot_from_input_line(line: str) -> Tuple[int, int]:
+    return tuple(int(c) for c in line.strip().split(","))
+
+
+class FoldDirection(Enum):
+    VERTICAL = auto()
+    HORIZONTAL = auto()
+
+
+@dataclass
+class Fold:
+    direction: FoldDirection
+    position: int
+
+    @classmethod
+    def from_input_line(cls, line: str) -> "Fold":
+        parts: List[str] = line.strip().split(" ")[2].split("=")
+        direction: FoldDirection = FoldDirection.VERTICAL if parts[
+            0
+        ] == "x" else FoldDirection.HORIZONTAL
+        return cls(direction, int(parts[1]))
+
+
+dots: Set[Tuple[int, int]]
+folds: List[Fold]
+with open(INPUT_FILE_NAME, "r") as input_file:
+    parts: List[str] = input_file.read().split("\n\n")
+    dots = set(dot_from_input_line(line) for line in parts[0].strip().split("\n"))
+    folds = [Fold.from_input_line(line) for line in parts[1].strip().split("\n")]
+
+for fold in folds:
+    new_dots: Set[Tuple[int, int]] = set()
+    for dot in dots:
+        if fold.direction == FoldDirection.HORIZONTAL:
+            if dot[1] < fold.position:
+                new_dots.add(dot)
+            else:
+                new_dots.add((dot[0], 2 * fold.position - dot[1]))
+        else:
+            if dot[0] < fold.position:
+                new_dots.add(dot)
+            else:
+                new_dots.add((2 * fold.position - dot[0], dot[1]))
+    dots = new_dots
+
+x_max = max(dot[0] for dot in dots)
+y_max = max(dot[1] for dot in dots)
+grid: List[str] = [
+    "".join(["#" if (x, y) in dots else " " for x in range(x_max + 1)])
+    for y in range(y_max + 1)
+]
+
+for line in grid:
+    print(line)
